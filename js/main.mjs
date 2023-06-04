f0a0f489103e62d6f412c61bdd687f11363311c8
yang <javayanglei2001@126.com>
2023-06-04 12:48:57 +0800

    初始化提交

--- js/main.mjs
+++ js/main.mjs
@@ -2,94 +2,68 @@ import * as Vue from './node_modules/vue3/vue.esm-browser.mjs';
 import * as Element from './node_modules/element-plus/index.full.mjs';
 import {taikoBase64Str} from "./taiko.mjs";
 
-// import ref
-let {ref} = Vue;
-let {watchEffect} = Vue;
+// import
+let {ref, onMounted, onUnmounted} = Vue;
 
-const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const numMin = ref(5);
-
-const numMax = ref(10);
-
-
-// 本数
-const currentBen = ref(10);
-const minBen = ref(0);
-const maxBen = ref(1000);
-
-// 记录已经打过的
-let countBen = ref(0);
-
-
-// 计算 timeOut 的间隔时间
-const timeOut = ref(getRandomInt(numMin.value * 1000, numMax.value * 1000));
-
-const switchValue = ref(false);
-
-// 记录 太鼓的 Audio 对象
-const taikoAudio = new Audio("data:audio/wav;base64," + taikoBase64Str());
-
-// 记录定时任务
-let timeoutId;
-
-const switchChange = (val) => {
-    if (val === false) {
-        clearTimeOutTask()
-    } else {
-        doTask();
-    }
-}
-
-const clearTimeOutTask = () => {
-    if (timeoutId) {
-        clearTimeout(timeoutId);
-        console.log('清理定时任务');
-    }
-}
-
-
-// 播放一个 wav 文件
-const playTaiko = () => {
-    // 本数 ++
-    countBen.value = countBen.value + 1;
-    taikoAudio.play().then(() => console.log('Playback started'));
-}
-
-const doTask = () => {
-    if (switchValue.value) {
-        console.log("执行延迟任务");
-        playTaiko();
-        timeOut.value = getRandomInt(numMin.value * 1000, numMax.value * 1000);
-        console.log(`当前的延时任务时间为 ${timeOut.value}`);
-        timeoutId = setTimeout(doTask, timeOut.value);
-    }
-
-}
 
 let app = Vue.createApp({
     setup() {
-        watchEffect(() => {
-            timeOut.value = getRandomInt(numMin.value * 1000, numMax.value * 1000);
-            clearTimeOutTask();
-            doTask();
-        })
-        watchEffect(() => {
-            if (countBen.value === currentBen.value) {
-                countBen.value = 0;
-                switchValue.value = false;
-                // 弹出窗口
-                Element.ElMessageBox.alert('恭喜你 🎉，当前训练已完成 ✅', 'Title', {
-                    confirmButtonText: '确认',
-                })
+        const fileName = ref(null);
 
+        const canvas = ref(HTMLCanvasElement);
+
+        const result = ref("");
+
+
+        onMounted(async () => {
+            canvas.value = document.getElementById("canvas");
+        });
+
+        onUnmounted(() => {
+            console.log("delete rec");
+        });
+
+        const uploadImg = () => {
+            /**
+             * 这里由于操作是绑定在 el-input 上；因此需要在内部重新获取 input 再拿到 file
+             */
+            const reader = new FileReader();
+            // 用于展示
+            const showImg = document.getElementById("show-img");
+            // 用于识别
+            const rawImg = document.getElementById("raw-img");
+            const inputElement = document
+                .getElementsByClassName("el-input")[0]
+                .getElementsByTagName("input")[0];
+
+            try {
+                const file = inputElement.files[0];
+                reader.onload = () => {
+                    showImg.src = URL.createObjectURL(file);
+                    rawImg.src = URL.createObjectURL(file);
+                };
+                reader.readAsDataURL(file);
+            } catch (err) {
+                console.error(err);
             }
-        })
-        return {numMax, numMin, switchValue, switchChange, currentBen, minBen, maxBen, countBen}
+        };
+
+        const recognize = async (image, options) => {
+            // 获取图像识别的结果
+            return {text: ["1", "2", "3"], points: "ok"};
+        }
+
+        const predict = async () => {
+            const img = document.getElementById("raw-img");
+            const res = await recognize(img, {canvas: canvas.value});
+            console.log(res);
+            if (res.text?.length) {
+                // 页面展示识别内容
+                result.value = res.text.reduce((total, cur) => total + `<p>${cur}</p>`);
+            }
+        };
+
+        return {fileName,uploadImg,predict,result}
     },
 });
 
